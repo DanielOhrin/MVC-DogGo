81f3f891834fe56cd90d61bb8180f8af2f27c18d
Daniel Ohrin <xqiam@outlook.com>
2023-01-06 15:50:53 -0600

    Completed Exercise 1&2

---
+++ DogGo/Interfaces/IWalkRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using DogGo.Models;
+
+namespace DogGo.Interfaces
+{
+    public interface IWalkRepository
+    {
+        List<Walk> GetWalksByWalkerId(int walkerId);
+        void CreateWalk(Walk walk);
+    }
+}
---
+++ DogGo/Models/Walk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogGo.Models
+{
+    public class Walk
+    {
+        private TimeSpan _ts => TimeSpan.FromSeconds(Duration);
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public int WalkerId { get; set; }
+        public Walker Walker { get; set; }
+        public int DogId { get; set; }
+        public Dog Dog { get; set; }
+        public string MinuteString => _ts.ToString(@"mm' min'");
+        public List<Dog> Dogs { get; set; }
+    }
+}
